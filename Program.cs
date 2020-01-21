8a88965d7f79ace59cfca04895ee00bb5a0118c5
Jacob Levin <yacob.levin3@gmail.com>
2020-01-21 17:48:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace My_Github_Learning_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //displays the text in the console
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("HEY YOU GUYS");
+            Console.WriteLine("is water wet?");
+            Console.WriteLine("if the earth is so big why wont it fight me?");
+        }
+    }
+}
