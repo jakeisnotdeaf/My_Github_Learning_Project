f3f9797d8eeb383b20324d04f0397e9a74d39834
Jacob Levin <yacob.levin3@gmail.com>
2020-01-23 17:27:12 -0500

    added comment

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace My_Github_Learning_Project
         {
             //displays the text in the console for the fun
             Console.WriteLine("Hello World!");
+
+            // Display the "Hey you guys" message
             Console.WriteLine("HEY YOU GUYS");
             Console.WriteLine("is water wet?");
             Console.WriteLine("if the universe is so big why wont it fight me?");
